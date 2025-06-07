4122f447da5c4ec5095529b438c08bae3c5d06ab
DS-J-L <ds4828@naver.com>
2025-06-07 20:40:19 +0900

    cache changed
    idk but somewhat changed.

--- BackEnd/modules/common/cache_util.py
+++ BackEnd/modules/common/cache_util.py
@@ -1,14 +1,18 @@
 from uuid import uuid4
 import requests
 from components.cache_data import CacheData
+from fastapi.encoders import jsonable_encoder
 
-def save_data(payload, backend_url="http://localhost:3000/api/cache/save") -> str:
-    key = uuid4().hex
+def save_data(key=None, payload=None, backend_url="http://localhost:3000/api/cache/save") -> str:
+    if not key:
+        key = uuid4().hex
+
+    serialized_payload = jsonable_encoder(payload)
 
     response = requests.post(
         backend_url,
         params={"key":key},
-        json=payload
+        json={"value":serialized_payload}
     )
 
     if response.status_code != 200:
@@ -22,6 +26,6 @@ def load_data(key:str, backend_url="http://localhost:3000/api/cache/get") -> Cac
     )
 
     if response.status_code != 200:
-        raise Exception(f"Failed to load POIs: {response.text}")
+        raise Exception(f"Failed to load data: {response.text}")
     data = response.json()
-    return data["value"]
+    return data["value"]["data"]
